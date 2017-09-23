fad71ed1cd8db804474f37729146f324ce9ed5f5
JaredDB <jbrower@oswego.edu>
2017-09-23 15:13:32 -0400

    main page

--- musiccatalog/urls.py
+++ musiccatalog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from main import views
 
 urlpatterns = [
-    url(r'^main/', include('main.urls')),
+    url(r'^$', views.index, name='site_index'),
+    #url(r'^main/', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
