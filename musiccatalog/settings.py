6d49f6c26bb5b6fdc9c0450660bc29057308218e
Jared <jareddavidbrower@gmail.com>
2017-10-30 19:14:20 -0400

    database setup

--- musiccatalog/settings.py
+++ musiccatalog/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DB_NAME = os.environ['MUSICCATALOG_NAME']
+DB_USER = os.environ['MUSICCATALOG_USER']
+DB_PASS = os.environ['MUSICCATALOG_PASS']
 
 # Application definition
 
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'musiccatalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
